961800aff40d852aa9fcc3734a6c9e2a462d39e0
JackCarson3 <jcarson25@liberty.edu>
2022-12-27 12:03:18 -0500

    Loaders

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,25 +1,27 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
-    <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
-        <g transform="translate(-5, -5)">
-          <path
-            d="M38 800v-42h13q33 0 57.5 -11.5t24.5 -55.5v-491q0 -47 -24 -59.5t-58 -12.5h-13v-42h504l5 175h-52l-5 -43q-4 -33 -23.5 -57.5t-64.5 -24.5h-168v266h241v49h-241v299h193q47 0 68.5 -24.5t26.5 -57.5l7 -43h52l-7 175h-536z"
-            fill="currentColor"
-          />
-        </g>
-        <polygon
-          id="Shape"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <title>Loader Logo</title>
+    <g>
+      <g id="W" transform="translate(20.000000, 29.000000)">
+        <path
+          d="M0.792614 44V0.363635H30.196V7.97017H10.0185V18.3679H28.6832V25.9744H10.0185V36.3935H30.2812V44H0.792614Z"
+          fill="currentColor"
         />
       </g>
+      <path
+        stroke="currentColor"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
+      />
     </g>
   </svg>
 );
