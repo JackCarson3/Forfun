884b5abfe9e04b804cf5053ef0e49fb3466e6225
JackCarson3 <jcarson25@liberty.edu>
2022-12-27 00:18:42 -0500

    Makin it bigger

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -4,8 +4,8 @@ const IconLogo = () => (
   <svg
     version="1.0"
     xmlns="http://www.w3.org/2000/svg"
-    width="1080.000000pt"
-    height="1080.000000pt"
+    width="2160.000000pt"
+    height="2160.000000pt"
     viewBox="0 0 2160.000000 2160.000000"
     preserveAspectRatio="xMidYMid meet">
     <g
